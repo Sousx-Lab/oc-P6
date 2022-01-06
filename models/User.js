21dc2d2c3ade20aa843ba7d8a3dc6e84401c959d
Nassim Hattab <nssmhttb@gmail.com>
2022-01-06 15:14:21 +0100

    feat: User model email validator

--- models/User.js
+++ models/User.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+const {isEmail} = require('validator');
 
 const userSchema = mongoose.Schema({
-    email: {type: String, unique: true, required: true},
-    password: {type: String, required: true}
+    email: {
+        type: String,
+        required: [true, 'Email address is required'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: [isEmail, "Please enter a valid email"],
+    },
+    password: {
+        type: String, 
+        required: true, 
+        trim: true
+    }
 });
 
 userSchema.plugin(uniqueValidator);
