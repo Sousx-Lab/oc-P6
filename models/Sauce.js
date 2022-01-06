f8d9d9914b8dca12b8116476dd3a13dfd0cb98df
Nassim Hattab <nssmhttb@gmail.com>
2022-01-06 15:13:51 +0100

    feat: sauce schema sanitizer

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
+const sanitizer = require('mongoose-sanitize');
 
 const sauceSchema = mongoose.Schema({
     userId: {type: String, required: true},
-    name: {type: String, required: true},
-    manufacturer: {type: String},
-    description: {type: String, required: true},
-    mainPepper: {type: String, required: true},
-    imageUlr: {type: String, required: true},
-    heat: {type: Number, required: true},
+    name: {type: String, required: true, trim: true,},
+    manufacturer: {type: String, trim: true},
+    description: {type: String, required: true, trim: true},
+    mainPepper: {type: String, required: true,},
+    imageUlr: {type: String, required: true, trim: true},
+    heat: {type: Number, required: true, trim: true},
     likes: {type: Number},
     dislikes: {type: Number},
     usersLiked: {type: Array},
     usersDisliked: {type: Array}
 });
-
+sauceSchema.plugin(sanitizer);
 module.exports = mongoose.model('Sauce', sauceSchema);
