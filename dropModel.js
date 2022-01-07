9f197b8dbbe894b2b25769798ab1fa5579c64a5d
Nassim Hattab <nssmhttb@gmail.com>
2022-01-07 11:11:34 +0100

    refactor: drop mongodb model script

--- dropModel.js
+++ dropModel.js
@@ -2,19 +2,25 @@ require('dotenv-flow').config();
 const dataBase = require('./services/database/database');
 dataBase.connect();
 
-dropModel = function(modelName){
-    const model = require(`./models/${modelName}`)
-    if(model){
-        model.collection.drop()
+
+const arg = process.argv.slice(2)[0]
+if(undefined === arg){
+    console.log("No model argument","\n","Exemple to drop User model: node dropModel User ");
+    process.exit(0);
+}
+
+const model = require(`./models/${arg}`)
+    if(!model){
+        console.log(`${model} model not found`);
+        process.exit(0);
+    }
+    arg.collection.drop()
         .then(() => {
-            console.log(`Model: ${modelName} dropper`);
+            console.log(`Model: ${arg} dropper`);
             process.exit(0);
         })
         .catch((error) =>{
             console.log(error)
             process.exit(1)
-        })
-    }
-}
-
-dropModel(process.argv.slice(2)[0])
+    })
+    process.exit(0);
