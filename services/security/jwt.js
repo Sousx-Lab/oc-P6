ad6e84d3257ab386dfb2fe4a0f594a22aac326b5
Nassim Hattab <nssmhttb@gmail.com>
2022-01-07 12:13:49 +0100

    feat: auth middleware

--- services/security/jwt.js
+++ services/security/jwt.js
@@ -2,14 +2,25 @@ const jwt = require('jsonwebtoken');
 
 exports.jwtSign = function(user){
     if(!user){
-        return null;
+        throw 'User not found';
     }
     return {
         userId: user._id,
         token: jwt.sign(
             {userId: user._id},
             process.env.JWT_TOKEN_SECRET,
-            {expiresIn: '24h'}
+            {expiresIn: process.env.JWT_TOKEN_EXPIRES_IN}
         )
     };
 };
+
+/**
+ * @param {string} token 
+ * @returns {any}
+ */
+exports.jwtVerify = function(token){
+    if(!token){
+        throw 'Token not found'
+    }
+    return jwt.verify(token, process.env.JWT_TOKEN_SECRET)
+}
