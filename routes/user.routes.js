098c20f269296a5c084cf44a4004a7688b2974d0
Nassim Hattab <nssmhttb@gmail.com>
2022-01-07 18:18:43 +0100

    refactor: user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
+const sauceController = require('../controllers/sauce.controller');
 
+/** Authentification Routes */
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
 
