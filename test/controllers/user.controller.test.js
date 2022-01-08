2dbf3e41c5a4577d0e21c3417a0ae35c44d391e1
Nassim Hattab <nssmhttb@gmail.com>
2022-01-09 00:33:45 +0100

    test: login test

--- test/controllers/user.controller.test.js
+++ test/controllers/user.controller.test.js
@@ -3,6 +3,8 @@ const request = require('supertest');
 const httpResponse = require('../../middleware/http/http.response');
 const dataBase = require('../../middleware/database/database');
 const User = require('../../models/User');
+const bcrypt = require('bcrypt');
+const { promise } = require('bcrypt/promises');
 
 const userRoute = {
     signup: "/api/auth/signup",
@@ -14,8 +16,11 @@ beforeAll(async () =>{
     await dataBase.dropModel(User);
 });
 
+/** 
+ * SignUp Tests  
+ */
 describe('sign up', () => {
-    it('returns status code 400 if email is blank', async () => {
+    it('returns status code 400 if email is empty', async () => {
         const res = await request(app)
         .post(userRoute.signup)
         .send({
@@ -57,8 +62,63 @@ describe('sign up', () => {
    
 });
 
+/**
+ * Login Tests 
+*/
+describe('login', () => {
+    
+    it("returns status code 401 if user email not found", async () =>{
+        const res = await request(app)
+        .post(userRoute.login)
+        .send({
+            email: '',
+            password: 'password'
+        });
+        expect(res.statusCode).toEqual(httpResponse.HTTP_UNAUTHORIZED);
+
+    });
+
+    it("returns status code 401 if password not match", async () =>{
+        const res = await request(app)
+        .post(userRoute.login)
+        .send({
+            email: 'email@test.com',
+            password: 'notMatchPassword'
+        });
+        expect(res.statusCode).toEqual(httpResponse.HTTP_UNAUTHORIZED);
+
+    });
+
+    it("returns status code 200 and JWToken if user succesfully authentificated", async () =>{
+        await dataBase.dropModel(User).then(() => {
+            bcrypt.hash('passwordTest', 10)
+            .then(hash => {
+                const user = new User({
+                email: "email@test.com",
+                password: hash
+            });
+            user.save()
+                .then(() => true)
+                .catch(error =>  console.log(error))
+            }).catch(error => console.log(error));
+            
+            async() => {
+                const res = await request(app)
+                .post(userRoute.login)
+                .send({
+                email: 'email@test.com',
+                password: 'passwordTest'
+            });
+            
+            expect(res.statusCode).toEqual(httpResponse.HTTP_OK);
+            }
+
+        });
+    });
+
+});
  /** Closing the DB connection allows Jest to exit successfully.*/
 afterAll(async () =>{
     await dataBase.close();
-  });
+});
   
