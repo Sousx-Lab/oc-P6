029e8a6da02d5a9844e3806f6ded54cf1f88bf15
Nassim Hattab <nssmhttb@gmail.com>
2022-01-11 16:23:21 +0100

    refactor: get all sauces

--- controllers/sauce.controller.js
+++ controllers/sauce.controller.js
@@ -2,13 +2,14 @@ const Response = require('../middleware/http/http.response');
 const Sauce = require('../models/Sauce');
 
 exports.getAllSauces = (req, res, next) => {
-    res.status(Response.HTTP_OK).json({message: 'sauce'})
+    Sauce.find()
+    .then((sauce) => res.status(Response.HTTP_OK).json(sauce))
+    .catch(error => res.status(Response.HTTP_SERVER_ERROR).json({error}))
 }
 
 exports.getOneSauce = (req, res, next) => {
     res.status(Response.HTTP_OK).json({message: 'sauce'})
 }
-
 
 exports.createSauce = (req, res, next) => {
     res.status(Response.HTTP_CREATED).json({message: 'sauce'})
@@ -18,11 +19,9 @@ exports.modifySauce = (req, res, next) => {
     res.status(Response.HTTP_CREATED).json({message: 'sauce'})
 }
 
-
 exports.deleteSauce = (req, res, next) => {
     res.status(Response.HTTP_CREATED).json({message: 'sauce'})
 }
-
 
 exports.ratingSauce = (req, res, next) => {
     res.status(Response.HTTP_CREATED).json({message: 'sauce'})
