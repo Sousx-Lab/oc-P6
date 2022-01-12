34dbccd7e1ea75ec75bfd0b1172ef6dd693efbcc
Nassim Hattab <nssmhttb@gmail.com>
2022-01-12 15:22:46 +0100

    refactor: delete unused method

--- middleware/database/database.js
+++ middleware/database/database.js
@@ -15,20 +15,5 @@ const dataBase = {
         .then(() => console.log('Connection database closed'))
         .catch(error => console.log(error));
     },
-
-    deleteData: async(model) => {
-        if(!model.prototype instanceof mongoose.Model){
-          console.log('Argument model is not a mongoose model');
-          return;
-        }
-        await model.collection.deleteMany({})
-        .then(() => {
-        console.log(`Model ${model.modelName} dropped`);
-    })
-    .catch((error) =>{
-        console.log(error)
-        process.exit(1)
-    })
-    } 
 }
 module.exports = dataBase
