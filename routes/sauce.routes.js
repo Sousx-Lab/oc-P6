9ee093769ca40743c96e3fc80134cfb467557dc9
Nassim Hattab <nssmhttb@gmail.com>
2022-01-13 15:59:36 +0100

    feat: add auth middleware to secure routes

--- routes/sauce.routes.js
+++ routes/sauce.routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const sauceController = require('../controllers/sauce.controller');
+const auth = require('../middleware/security/auth');
 
 /** Sauce Routes */
-router.get('/', sauceController.getAllSauces);
+router.get('/', auth, sauceController.getAllSauces);
 router.post('/', sauceController.createSauce);
-router.get('/:id', sauceController.getOneSauce);
-router.put('/:id', sauceController.modifySauce);
-router.delete('/:id', sauceController.deleteSauce);
-router.post('/:id/like', sauceController.ratingSauce);
+router.get('/:id', auth, sauceController.getOneSauce);
+router.put('/:id', auth, sauceController.modifySauce);
+router.delete('/:id', auth, sauceController.deleteSauce);
+router.post('/:id/like', auth, sauceController.ratingSauce);
 
 module.exports = router;
