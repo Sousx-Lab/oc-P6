6e2ba4e56d8e3b77538ed2ebeaec9b6dfdda8a24
Nassim Hattab <nssmhttb@gmail.com>
2022-01-16 15:39:45 +0100

    refactor: console color

--- middleware/database/database.js
+++ middleware/database/database.js
@@ -6,13 +6,13 @@ const dataBase = {
                 useNewUrlParser: true,
                 useUnifiedTopology: true
           })
-        .then(()=> console.log("\x1b[42m", 'Connexion à MongoDB réussie !', "\x1b[40m\n"))
-        .catch(() => console.log("\x1b[41m", 'Connexion à MongoDB échouée !', "\x1b[41m"));
+        .then(()=> console.log("\x1b[42m", 'Connexion à MongoDB réussie !', "\x1b[40m\n","\n"))
+        .catch(() => console.log("\x1b[41m", 'Connexion à MongoDB échouée !', "\x1b[41m\n","\n"));
     },
 
     disconnect: async () =>{
         await mongoose.connection.close()
-        .then(() => console.log('Connection database closed'))
+        .then(() => console.log("\x1b[42m", "Connection database closed", "\x1b[40m\n","\n"))
         .catch(error => console.log(error));
     },
 }
