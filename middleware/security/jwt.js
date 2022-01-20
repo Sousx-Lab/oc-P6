76b01851ded5ad0a81eb36de192aa619371dbfd6
Nassim Hattab <nssmhttb@gmail.com>
2022-01-20 16:17:34 +0100

    refactor: try catch bloc

--- middleware/security/jwt.js
+++ middleware/security/jwt.js
@@ -1,34 +1,43 @@
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
+
 /**
  * @param {object} user 
  * @returns {object|Error}
  */
 exports.jwtSign = function (user) {
-    if(!user instanceof mongoose.model){
-        throw 'User not found';
+    try {
+        if(!user instanceof mongoose.model){
+            throw 'User not found';
+        }
+        return {
+            userId: user._id,
+            token: jwt.sign({
+    
+                    userId: user._id,
+                    email: user.email
+                },
+                process.env.JWT_TOKEN_SECRET, {
+                    expiresIn: process.env.JWT_TOKEN_EXPIRES_IN
+                }
+            )
+        };
+    } catch (error) {
+        throw error;
     }
-    return {
-        userId: user._id,
-        token: jwt.sign({
-
-                userId: user._id,
-                email: user.email
-            },
-            process.env.JWT_TOKEN_SECRET, {
-                expiresIn: process.env.JWT_TOKEN_EXPIRES_IN
-            }
-        )
-    };
 };
 
 /**
  * @param {string} token 
- * @returns {object|null}
+ * @returns {object|Error}
  */
 exports.jwtVerify = function (token) {
-    if (!token) {
-        throw 'Token not found'
+    try {
+        if (!token) {
+            throw 'Token not found';
+        }
+        return jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+    } catch (error) {
+        throw error;
     }
-    return jwt.verify(token, process.env.JWT_TOKEN_SECRET)
 }
